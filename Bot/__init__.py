1c1cc3cc960c9c6ead7955484a4db977866402e1
Mason B <legotrainkid@gmail.com>
2021-07-04 23:25:33 -0400

    Begin Groundwork

--- Bot/__init__.py
+++ Bot/__init__.py
@@ -1,5 +1,13 @@
 import discord
 
+import Bot.settings
+
+
 class Bot(discord.Client):
     def start_game(self):
         pass
+
+    def on_message(self, message):
+        if message.content[0:len(settings.PREFIX)] != settings.PREFIX or message.author == self.user:
+            return
+        command = message.content[len(settings.PREFIX):].split(" ")
