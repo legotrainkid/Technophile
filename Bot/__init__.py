8b1b403775faffa786175624f3d10fa50f317040
Mason B <legotrainkid@gmail.com>
2021-07-06 14:21:24 -0400

    Add Server Setup Command

--- Bot/__init__.py
+++ Bot/__init__.py
@@ -1,13 +1,37 @@
 import discord
+import asyncio
 
 import Bot.settings
 
 
 class Bot(discord.Client):
+    def __init__(self):
+        super().__init__()
+        self.commands = {}
+
     def start_game(self):
         pass
 
-    def on_message(self, message):
+    async def on_message(self, message):
         if message.content[0:len(settings.PREFIX)] != settings.PREFIX or message.author == self.user:
             return
-        command = message.content[len(settings.PREFIX):].split(" ")
+        arguments = message.content[len(settings.PREFIX):].split(" ")
+        if arguments[0] in self.commands:
+            await self.commands[arguments[0]](arguments, message)
+
+    async def setup_server(self, arguments : list, message : discord.Message):
+        # Command: ?setup number_of_channels
+        server = message.guild
+        ready_cat = await server.create_category_channel("Available Computer Channels")
+        new_server = {}
+        for i in range(int(arguments[1])):
+            name = "Computer-" + str(i)
+            new_channel = await server.create_text_channel(name, category=ready_cat)
+            role_name = name+"-User"
+            new_role = await server.create_role(name=role_name)
+            await settings.NormalOverWrite(new_channel, new_role)
+            new_server[new_channel.id] = new_role.id
+        print(new_server)
+        settings.SERVERS[server.id] = new_server
+        await message.channel.send("Your server is now set up!")
+
--- Bot/settings.py
+++ Bot/settings.py
@@ -1,1 +1,19 @@
+import discord
+import asyncio
+
 PREFIX = "?"
+SERVERs = {}
+
+
+def NormalOverWrite(channel, channel_role):
+    over = discord.PermissionOverwrite()
+    over.general()
+    await channel.set_permisions(channel.guild.default_role, overwrite=over)
+    await channel.set_permisions(channel_role, overwrite=over)
+
+
+async def ClosedOverWrite(channel):
+    over = discord.PermissionOverwrite()
+    over.pair(view_channel=False)
+    await channel.set_permisions(channel.guild.default_role, overwrite=over)
+
