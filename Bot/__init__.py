f32f7bb6047651f6514097e5bc594b4498a7f018
Mason B <legotrainkid@gmail.com>
2021-07-07 14:14:55 -0400

    Add ?use and ?shutdown commands

--- Bot/__init__.py
+++ Bot/__init__.py
@@ -7,16 +7,23 @@ import Bot.settings
 class Bot(discord.Client):
     def __init__(self):
         super().__init__()
-        self.commands = {}
+        self.commands = {"setup" : self.setup_server, "use" : self.claim_channel, "shutdown" : self.unclaim_channel}
 
     def start_game(self):
-        pass
+        self.run(settings.LoadToken())
+
+    async def on_ready(self):
+        print("Ready")
 
     async def on_message(self, message):
+        print(message.content)
         if message.content[0:len(settings.PREFIX)] != settings.PREFIX or message.author == self.user:
+            print("Command not recognized")
             return
         arguments = message.content[len(settings.PREFIX):].split(" ")
+        print(arguments[0])
         if arguments[0] in self.commands:
+            print("Running Command: " + arguments[0])
             await self.commands[arguments[0]](arguments, message)
 
     async def setup_server(self, arguments : list, message : discord.Message):
@@ -25,7 +32,7 @@ class Bot(discord.Client):
         ready_cat = await server.create_category_channel("Available Computer Channels")
         new_server = {}
         for i in range(int(arguments[1])):
-            name = "Computer-" + str(i)
+            name = "Computer-" + str(i+1)
             new_channel = await server.create_text_channel(name, category=ready_cat)
             role_name = name+"-User"
             new_role = await server.create_role(name=role_name)
@@ -34,4 +41,30 @@ class Bot(discord.Client):
         print(new_server)
         settings.SERVERS[server.id] = new_server
         await message.channel.send("Your server is now set up!")
+
+    async def claim_channel(self, arguments, message):
+        # Command: ?use
+        server = message.guild
+        print(3)
+        print(server.id)
+        print(settings.SERVERS)
+        if server.id in settings.SERVERS:
+            print(1)
+            if message.channel.id in settings.SERVERS[server.id]:
+                print(2)
+                await message.author.add_roles(server.get_role(settings.SERVERS[server.id][message.channel.id]))
+                await settings.ClosedOverWrite(message.channel)
+                await message.channel.send("You have claimed this channel!")
+
+    async def unclaim_channel(self, arguments, message):
+        server = message.guild
+        if server.id in settings.SERVERS:
+            print(1)
+            if message.channel.id in settings.SERVERS[server.id]:
+                print(2)
+                await message.author.remove_roles(server.get_role(settings.SERVERS[server.id][message.channel.id]))
+                await message.channel.purge(limit=1000)
+                await settings.NormalOverWrite(message.channel, server.get_role(settings.SERVERS[server.id][message.channel.id]))
+                await message.channel.send("Channel available to use! Type `?use` to claim this channel!")
+
 
--- Bot/settings.py
+++ Bot/settings.py
@@ -1,19 +1,44 @@
 import discord
 import asyncio
+import pickle
 
 PREFIX = "?"
-SERVERs = {}
+SERVERS = {821517617016733736 : {862374893503905802: 862374893932249179, 862374895651389502: 862374896159031306, 862374897975558164: 862374898374541342}}
 
 
-def NormalOverWrite(channel, channel_role):
+async def NormalOverWrite(channel, channel_role):
     over = discord.PermissionOverwrite()
-    over.general()
-    await channel.set_permisions(channel.guild.default_role, overwrite=over)
-    await channel.set_permisions(channel_role, overwrite=over)
+    over.view_channel = True
+    await channel.set_permissions(channel.guild.default_role, overwrite=over)
+    await channel.set_permissions(channel_role, overwrite=over)
 
 
 async def ClosedOverWrite(channel):
+    # Function to hide the channel from the default role
+    # Create a permission overwrite
     over = discord.PermissionOverwrite()
-    over.pair(view_channel=False)
-    await channel.set_permisions(channel.guild.default_role, overwrite=over)
+    # Set the view_channel permission to false
+    over.view_channel = False
+    # Set the default role permissions to the created permissions
+    await channel.set_permissions(channel.guild.default_role, overwrite=over)
+
+
+def LoadToken():
+    # Used to load the token for the bot
+    with open("Bot\\token.txt", "r") as file:
+        # retrieve the token from the file
+        token = file.readline()
+    # return the token
+    return token
+
+
+def SaveServers():
+    pass
+
+
+def LoadServers():
+    with open("Bot\\servers.data") as file:
+        servers = pickle.load(file)
+
+    return servers
 
